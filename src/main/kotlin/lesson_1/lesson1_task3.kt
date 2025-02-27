a3dfbb2d04baac322ea3a3a39085b8b8909c00c5
Глеб Останин <gleb.ostanin2014@yandex.ru>
2025-02-27 18:27:15 +0500

    KS-1-3 change type to strings

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,16 +1,16 @@
 package org.example.lesson_1
 
 fun main() {
-    val year: Short = 1961
-    var hour: Byte = 9
-    var minute: Byte = 7
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
 
     println(year)
     println(hour)
     println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     print(hour)
     print(":")
