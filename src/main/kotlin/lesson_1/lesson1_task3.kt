5eb69cf622cd77b0fd1ee3cdcbab4606e7dfb01a
Глеб Останин <gleb.ostanin2014@yandex.ru>
2025-02-27 12:52:11 +0500

    KS-1-3 first launch time

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main() {
+    val year: Short = 1961
+    var hour: Byte = 9
+    var minute: Byte = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(":")
+    print(minute)
+}
