e64a84e8be73cdb21559d05ed1cf662611100d1b
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-03-04 15:43:22 +0500

    KS-3-5 refactor parsing

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -3,10 +3,10 @@ package org.example.lesson_3
 fun main() {
     val turnData = "D2-D4;0"
 
-    val turns = turnData.substringBefore(';').split("-")
+    val turns = turnData.split("-", ";")
     val from = turns[0]
     val to = turns[1]
-    val turnNumber = turnData.substringAfter(';').toInt()
+    val turnNumber = turns[2]
 
     println("From: $from")
     println("To: $to")
