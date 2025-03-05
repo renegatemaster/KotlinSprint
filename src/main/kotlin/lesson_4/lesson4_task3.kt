e8061f3dec22c070ba08af3f59ba92ce465841ad
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-03-05 12:14:41 +0500

    KS-4-3 refactor variables names

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -4,12 +4,12 @@ const val FINE_HUMIDITY = 20
 const val BAD_SEASON = "зима"
 
 fun main() {
-    val sunny = true
-    val awningIsOpen = true
+    val isSunny = true
+    val isAwningOpen = true
     val humidity = 20
     val season = "зима"
 
     println("Благоприятные ли условия сейчас для роста бобовых? ${
-        sunny && awningIsOpen && (humidity == FINE_HUMIDITY) && (season != BAD_SEASON)
+        isSunny && isAwningOpen && (humidity == FINE_HUMIDITY) && (season != BAD_SEASON)
     }")
 }
