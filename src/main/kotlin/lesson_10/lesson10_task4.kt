b18b81c9c6dbd1775ea911a140e8eb39e20ddcef
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-03-13 23:44:35 +0500

    KS-10-4 create infinite dice game

---
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -0,0 +1,50 @@
+package org.example.lesson_10
+
+import java.lang.Thread
+
+const val POSITIVE_ANSWER = "Да"
+
+fun rollTheDice() : Int {
+    Thread.sleep(1000)
+    return (1..6).random()
+}
+
+fun playTheGame() : Boolean {
+    println("Ход игрока...")
+    val userResult = rollTheDice()
+    println("Результат игрока: $userResult")
+
+    Thread.sleep(1000)
+
+    println("Ход компьютера...")
+    val machineResult = rollTheDice()
+    println("Результат компьютера: $machineResult")
+
+    Thread.sleep(1000)
+
+    if (userResult > machineResult) {
+        println("Победило человечество")
+        return true
+    } else if (userResult == machineResult) {
+        println("Победила дружба")
+        return false
+    } else {
+        println("Победила машина")
+        return false
+    }
+}
+
+fun main() {
+    var victoryCounter = 0
+    var playAgain: String
+
+    do {
+        val result = playTheGame()
+        if (result) victoryCounter++
+
+        println("Хотите бросить кости еще раз? Введите Да или Нет")
+        playAgain = readln()
+    } while (playAgain.equals(POSITIVE_ANSWER, ignoreCase = true))
+
+    println("Количество выигрышных партий: $victoryCounter")
+}
