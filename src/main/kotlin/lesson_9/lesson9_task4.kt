cb9f15dc36406cabc5ca52a50622285a0e4a9f00
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-03-13 19:30:43 +0500

    KS-9-4 refactor input

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -1,16 +1,8 @@
 package org.example.lesson_9
 
 fun main() {
-    var ingredientList: List<String>
+    println("Введите 5 ингредиентов через запятую (с пробелом):")
+    val ingredientsString = readln().split(", ").sorted()
 
-    while (true) {
-        println("Введите 5 ингредиентов через запятую (с пробелом):")
-        val ingredientsString = readln()
-
-        ingredientList = ingredientsString.split(", ")
-        if (ingredientList.size == 5) break else println("Неверное количество ингредиентов")
-    }
-
-    val sortedIngredientList = ingredientList.sorted()
-    println(sortedIngredientList.joinToString(", "))
+    println(ingredientsString.joinToString(", "))
 }
