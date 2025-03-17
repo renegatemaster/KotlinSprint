184fc0cb1903a7a8a670f04058af76be434d91ab
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-03-17 13:47:27 +0500

    KS-12-4 add init data print

---
+++ src/main/kotlin/lesson_12/lesson12_task4.kt
@@ -0,0 +1,35 @@
+package org.example.lesson_12
+
+class DayWeather(
+    _daytimeTemp: Int,
+    _nighttimeTemp: Int,
+    _hasPrecipitation: Boolean,
+) {
+    private val kelvinOffset = 273
+
+    val daytimeTemp = _daytimeTemp - kelvinOffset
+    val nighttimeTemp = _nighttimeTemp - kelvinOffset
+    val hasPrecipitation = _hasPrecipitation
+
+    init {
+        this.printData()
+    }
+
+    fun printData() {
+        println(
+            """
+            Температура днём: $daytimeTemp °С
+            Температура ночью: $nighttimeTemp °С
+            Наличие осадков: $hasPrecipitation
+            """.trimIndent()
+        )
+    }
+}
+
+fun main() {
+    val dayOne = DayWeather(
+        _daytimeTemp = 300,
+        _nighttimeTemp = 290,
+        _hasPrecipitation = false,
+    )
+}
