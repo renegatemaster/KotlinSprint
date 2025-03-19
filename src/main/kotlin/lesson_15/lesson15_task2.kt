577c3a804cabd22bb0a497691da644c4af90afac
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-03-19 20:50:14 +0500

    KS-15-2 create weather server

---
+++ src/main/kotlin/lesson_15/lesson15_task2.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_15
+
+abstract class WeatherStationStats(val value: Double)
+
+class Temperature(value: Double) : WeatherStationStats(value) {
+    override fun toString(): String {
+        return "Температура: $value°C"
+    }
+}
+
+class PrecipitationAmount(value: Double) : WeatherStationStats(value) {
+    override fun toString(): String {
+        return "Количество осадков: $value мм"
+    }
+}
+
+class WeatherServer {
+    fun sendMessage(forecast: WeatherStationStats) { println(forecast) }
+}
+
+fun main() {
+    WeatherServer().let {
+        it.sendMessage(Temperature(value = 15.0))
+        it.sendMessage(PrecipitationAmount(value = 0.3))
+    }
+}
